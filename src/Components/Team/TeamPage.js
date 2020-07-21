0556803829ffd09e6bdb194fdeaf6f0eab4e9f5d
George Gardner <gpgardner@yahoo.com>
2020-07-21 17:21:11 -0400

    removed old logic, added container

--- src/Components/Team/TeamPage.js
+++ src/Components/Team/TeamPage.js
@@ -9,6 +9,7 @@ import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
+import Container from "@material-ui/core/Container";
 
 const useStyles = makeStyles({
   table: {
@@ -16,56 +17,55 @@ const useStyles = makeStyles({
   },
 });
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [];
-
 export default function TeamPage(props) {
   const classes = useStyles();
-	
-	const {team} = props;
-	const teamName = Object.keys(team).toString();
-	const teamId = Object.values(team).toString();
 
-	
+  const { team } = props;
+  const teamName = Object.keys(team).toString();
+  const teamId = Object.values(team).toString();
+
   const [roster, setRoster] = useState([]);
-	
+
   useEffect(() => {
-		const callForPlayers = async () => {
-			const URL = `https://statsapi.web.nhl.com/api/v1/teams/${teamId}?expand=team.roster`
+    const callForPlayers = async () => {
+      const URL = `https://statsapi.web.nhl.com/api/v1/teams/${teamId}?expand=team.roster`;
       const response = await fetch(URL);
       const data = await response.json();
-      console.log('Use Effect from TeamPage ran again.')
+      console.log("Use Effect from TeamPage ran again.");
       setRoster(data.teams[0].roster.roster);
     };
     callForPlayers();
   }, [URL]);
 
-
   return (
-		<TableContainer component={Paper}>
-      <Table className={classes.table} aria-label={`table for nhl team ${teamName}`}>
-        <TableHead>
-          <TableRow>
-            <TableCell align="left">Jersey #</TableCell>
-            <TableCell>Player name</TableCell>
-            <TableCell align="right">Type</TableCell>
-            <TableCell align="right">Position</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {roster.map((player) => (
-            <TableRow key={player.person.id}>
-              <TableCell align="left">{player.jerseyNumber}</TableCell>
-              <TableCell component="th" scope="row">{player.person.fullName}</TableCell>
-              <TableCell align="right">{player.position.type}</TableCell>
-              <TableCell align="right">{player.position.name}</TableCell>
+    <Container>
+      <TableContainer component={Paper}>
+        <Table
+          className={classes.table}
+          aria-label={`table for nhl team ${teamName}`}
+        >
+          <TableHead>
+            <TableRow>
+              <TableCell align="left">Jersey #</TableCell>
+              <TableCell>Player name</TableCell>
+              <TableCell align="right">Type</TableCell>
+              <TableCell align="right">Position</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {roster.map((player) => (
+              <TableRow key={player.person.id}>
+                <TableCell align="left">{player.jerseyNumber}</TableCell>
+                <TableCell component="th" scope="row">
+                  {player.person.fullName}
+                </TableCell>
+                <TableCell align="right">{player.position.type}</TableCell>
+                <TableCell align="right">{player.position.name}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Container>
   );
 }
