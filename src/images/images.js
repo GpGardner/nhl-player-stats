148b362e9ca107d1a00ac40c100ff152f2a5e099
George Gardner <gpgardner@yahoo.com>
2020-07-23 12:41:52 -0400

    included team images, website images

---
+++ src/images/images.js
@@ -0,0 +1,34 @@
+// const images = './NHLTeamLogo/avalanche.png'
+
+// export default images;
+// // Hurricanes
+// // Blue Jackets
+// // Devils
+// // Islanders
+// // Rangers
+// // Flyers
+// // Penguins 
+// // Capitals
+// // Bruins
+// // Sabres
+// // Red Wings
+// // Panthers
+// // Canadiens
+// // Senators
+// // Lightning
+// // Maple Leafs
+// // Blackhawks
+// // Avalanche
+// // Stars
+// // Wild
+// // Predators
+// // Blues
+// // Jets
+// // Ducks
+// // Coyotes
+// // Flames
+// // Oilers
+// // Kings
+// // Sharks
+// // Canucks
+// // Golden Knights
