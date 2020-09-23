2833c7a97264c362d995a880e90020c05dc3eced
George Gardner <gpgardner@yahoo.com>
2020-09-23 19:15:37 -0400

    making different charts to select between

--- src/Components/Player/PlayerChart.js
+++ src/Components/Player/PlayerChart.js
@@ -6,6 +6,7 @@ import { Bar } from "react-chartjs-2";
 
 function Chart(props) {
   const [chartData, setChartData] = useState({});
+  const [statsType, setStatsType] = useState("basic");
 
   const { playerStats } = props;
 
@@ -90,123 +91,138 @@ function Chart(props) {
   });
 
   useEffect(() => {
-    setChartData({
-      labels: [...seasonsArray],
-      datasets: [
-        {
-          label: "Goals",
-          data: [...goalsArray],
-          backgroundColor: chooseBackgroundColor(),
-        },
-        {
-          label: "Assists",
-          data: [...assistsArray],
-          backgroundColor: chooseBackgroundColor(),
-        },
-        {
-          label: "Games",
-          data: [...gamesArray],
-          backgroundColor: chooseBackgroundColor(),
-        },
-        {
-          label: "Blocked",
-          data: [...blockedArray],
-          backgroundColor: chooseBackgroundColor(),
-        },
-        {
-          label: "Even Time On Ice",
-          data: [...evenTimeOnIceArray],
-          backgroundColor: chooseBackgroundColor(),
-        },
-        {
-          label: "Face Off Percentage",
-          data: [...faceOffPctArray],
-          backgroundColor: chooseBackgroundColor(),
-        },
+    if (statsType === "basic") {
+      setChartData({
+        labels: [...seasonsArray],
+        datasets: [
+          // {
+          //   label: "Games Played",
+          //   data: [...gamesArray],
+          //   backgroundColor: chooseBackgroundColor(),
+          // },
+          {
+            label: "Goals",
+            data: [...goalsArray],
+            backgroundColor: chooseBackgroundColor(),
+          },
+          {
+            label: "Assists",
+            data: [...assistsArray],
+            backgroundColor: chooseBackgroundColor(),
+          },
+          {
+            label: "Points",
+            data: [...pointsArray],
+            backgroundColor: chooseBackgroundColor(),
+          },
+          {
+            label: "Plus/Minus",
+            data: [...plusMinusArray],
+            backgroundColor: chooseBackgroundColor(),
+          },
+        ]})}
+    if (statsType === "even") {
+      setChartData({
+        labels: [...seasonsArray],
+        datasets: [
+          {
+            label: "Goals",
+            data: [...goalsArray],
+            backgroundColor: chooseBackgroundColor(),
+          },
+          {
+            label: "Assists",
+            data: [...assistsArray],
+            backgroundColor: chooseBackgroundColor(),
+          },
+          {
+            label: "Games",
+            data: [...gamesArray],
+            backgroundColor: chooseBackgroundColor(),
+          },
+          {
+            label: "Blocked",
+            data: [...blockedArray],
+            backgroundColor: chooseBackgroundColor(),
+          },
+          {
+            label: "Even Time On Ice",
+            data: [...evenTimeOnIceArray],
+            backgroundColor: chooseBackgroundColor(),
+          },
+          {
+            label: "Face Off Percentage",
+            data: [...faceOffPctArray],
+            backgroundColor: chooseBackgroundColor(),
+          },
 
-        {
-          label: "GWG",
-          data: [...gameWinningGoalsArray],
-          backgroundColor: chooseBackgroundColor(),
-				},
-				{
-          label: "Hits",
-          data: [...hitsArray],
-          backgroundColor: chooseBackgroundColor(),
-				},
-				{
-          label: "Over Time Goals",
-          data: [...overTimeGoalsArray],
-          backgroundColor: chooseBackgroundColor(),
-				},
-				{
-          label: "Penalty Minutes",
-          data: [...penaltyMinutesArray],
-          backgroundColor: chooseBackgroundColor(),
-				},
-				
-				{
-          label: "PIM",
-          data: [...pimArray],
-          backgroundColor: chooseBackgroundColor(),
-				},
-				{
-          label: "Plus/Minus",
-          data: [...plusMinusArray],
-          backgroundColor: chooseBackgroundColor(),
-				},
-				{
-          label: "Points",
-          data: [...pointsArray],
-          backgroundColor: chooseBackgroundColor(),
-				},
-				{
-          label: "PPG",
-          data: [...powerPlayGoalsArray],
-          backgroundColor: chooseBackgroundColor(),
-				},
-				{
-          label: "PPP",
-          data: [...powerPlayPointsArray],
-          backgroundColor: chooseBackgroundColor(),
-				},
-				{
-          label: "PP Time",
-          data: [...powerPlayTimeOnIceArray],
-          backgroundColor: chooseBackgroundColor(),
-				},
-				{
-          label: "SHG",
-          data: [...shortHandedGoalsArray],
-          backgroundColor: chooseBackgroundColor(),
-				},
-				{
-          label: "SHP",
-          data: [...shortHandedPointsArray],
-          backgroundColor: chooseBackgroundColor(),
-				},
-				{
-          label: "SH Time",
-          data: [...shortHandedTimeOnIceArray],
-          backgroundColor: chooseBackgroundColor(),
-				},
-				{
-          label: "Shot %",
-          data: [...shotPctArray],
-          backgroundColor: chooseBackgroundColor(),
-				},
-				{
-          label: "Total Shots",
-          data: [...shotsArray],
-          backgroundColor: chooseBackgroundColor(),
-				},
-				{
-          label: "Time On Ice",
-          data: [...timeOnIceArray],
-          backgroundColor: chooseBackgroundColor(),
-        },
-      ],
-    });
+          {
+            label: "GWG",
+            data: [...gameWinningGoalsArray],
+            backgroundColor: chooseBackgroundColor(),
+          },
+          {
+            label: "Hits",
+            data: [...hitsArray],
+            backgroundColor: chooseBackgroundColor(),
+          },
+          {
+            label: "Over Time Goals",
+            data: [...overTimeGoalsArray],
+            backgroundColor: chooseBackgroundColor(),
+          },
+          {
+            label: "Plus/Minus",
+            data: [...plusMinusArray],
+            backgroundColor: chooseBackgroundColor(),
+          },
+        ],
+      });
+    }
+    if (statsType === "power") {
+      setChartData({
+        labels: [...seasonsArray],
+        datasets: [
+          {
+            label: "PPG",
+            data: [...powerPlayGoalsArray],
+            backgroundColor: chooseBackgroundColor(),
+          },
+          {
+            label: "PPP",
+            data: [...powerPlayPointsArray],
+            backgroundColor: chooseBackgroundColor(),
+          },
+          {
+            label: "PP Time",
+            data: [...powerPlayTimeOnIceArray],
+            backgroundColor: chooseBackgroundColor(),
+          },
+          {
+            label: "SHG",
+            data: [...shortHandedGoalsArray],
+            backgroundColor: chooseBackgroundColor(),
+          },
+        ],
+      });
+    }
+    if (statsType === "short") {
+      setChartData({
+        labels: [...seasonsArray],
+        datasets: [
+          {
+            label: "Short Handed Points",
+            data: [...shortHandedPointsArray],
+            backgroundColor: chooseBackgroundColor(),
+          },
+          {
+            label: "SH Time",
+            data: [...shortHandedTimeOnIceArray],
+            backgroundColor: chooseBackgroundColor(),
+          },
+        ]
+      })}
+    console.log(chartData);
   }, []);
 
   return (
@@ -228,3 +244,34 @@ function Chart(props) {
 }
 
 export default Chart;
+
+// {
+//   label: "Penalty Minutes",
+//   data: [...penaltyMinutesArray],
+//   backgroundColor: chooseBackgroundColor(),
+// },
+
+// {
+//   label: "PIM",
+//   data: [...pimArray],
+//   backgroundColor: chooseBackgroundColor(),
+// },
+
+
+
+
+// {
+//   label: "Shot %",
+//   data: [...shotPctArray],
+//   backgroundColor: chooseBackgroundColor(),
+// },
+// {
+//   label: "Total Shots",
+//   data: [...shotsArray],
+//   backgroundColor: chooseBackgroundColor(),
+// },
+// {
+//   label: "Time On Ice",
+//   data: [...timeOnIceArray],
+//   backgroundColor: chooseBackgroundColor(),
+// },
